ca0edc891f278a0488c9518b6d3bea7c7ee9d517
Yurii Oborskiy <yuriioborskiy@gmail.com>
2022-10-09 14:56:33 +0300

    install jquery, slick, add indexjs

--- src/index.js
+++ src/index.js
@@ -14,3 +14,49 @@
         mobileMenuRef.classList.toggle("is-open");
     });
 })();
+
+import jQuery from "jquery";
+window.$ = window.jQuery = jQuery;
+
+
+$(document).ready(function () {
+    $('.slider').slick({
+        arrows: true,
+        dots: true,
+        adaptiveHeight: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        speed: 2000,
+        easing: `ease`,
+        infinite: true,
+        initialSlide: 0,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        pauseOnFocus: true,
+        pauseOnHover: true,
+        pauseOnDotsHover:true,
+        fade:true,
+    });
+    
+});
+
+$(document).ready(function () {
+    $('.slider2').slick({
+        arrows: true,
+        dots: true,
+        adaptiveHeight: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        speed: 2000,
+        easing: `ease`,
+        infinite: true,
+        initialSlide: 0,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        pauseOnFocus: true,
+        pauseOnHover: true,
+        pauseOnDotsHover:true,
+        fade:true,
+    });
+    
+});
