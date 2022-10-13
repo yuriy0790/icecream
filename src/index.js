26306f94472cfff3a3bcd7d177935a630a3bba80
Tetiana-V-Kovalenko <113469589+Tetiana-V-Kovalenko@users.noreply.github.com>
2022-10-13 15:55:57 +0300

    script for slow scroll

--- src/index.js
+++ src/index.js
@@ -70,4 +70,19 @@ document.addEventListener("DOMContentLoaded", function () {
 
 });
 
+const anchors = document.querySelectorAll('a[href*="#"]')
+
+for (let anchor of anchors) {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault()
+
+        const blockID = anchor.getAttribute('href').substr(1)
+
+        document.getElementById(blockID).scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        })
+    })
+}
+
 import './js/modals'
